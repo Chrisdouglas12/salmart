5ee7f91a8d6ac507fdac95c46492c87ceb11237c
ChrisDouglas <chrisdouglas1700@gmail.com>
2025-06-04 06:34:00 +0100

    added more catche pages

--- services/notificationUtils.js
+++ services/notificationUtils.js
@@ -49,7 +49,7 @@ async function sendFCMNotification(userId, title, body, data = {}, io, imageUrl 
       webpush: {
         headers: { Urgency: 'high' },
         notification: {
-          icon: profilePictureUrl || 'https://salmart.vercel.app/favicon.ico',
+          icon: profilePictureUrl || 'https://salmart.vercel.app/salmart-192x192.png',
           image: imageUrl || null,
           requireInteraction: true,
         },
